4b7be22808627781cbe75cd46954029f8115b61b
Hyu-waseda <okumura.hyu@akane.waseda.jp>
2022-10-22 17:59:39 +0900

    update: コード整形

--- main.py
+++ main.py
@@ -6,49 +6,51 @@ import numpy as np
 import datetime
 
 # スクショを取る基準
-screenShot_rate=0.05
+screenShot_rate = 0.05
 # 何秒おきにスクショをとるか
-sleep_time=3
+sleep_time = 3
+
 
 def main():
-    #最後に保存したフレーム
+    # 最後に保存したフレーム
     old_img = ScreenShot()
 
     count = 0
     while True:
-        #現在のフレーム
+        # 現在のフレーム
         img = ScreenShot()
 
-        #変化量計算
-        rate = ImageChangeRate(old_img,img)
+        # 変化量計算
+        rate = ImageChangeRate(old_img, img)
         print(rate)
 
-        #大きな変化があった場合
+        # 大きな変化があった場合
         if rate > screenShot_rate:
-            #画像を保存
-            cv2.imwrite("imgs/"+str(datetime.datetime.now())+str(count)+".png",img)
+            # 画像を保存
+            cv2.imwrite("imgs/"+str(datetime.datetime.now()) +
+                        str(count)+".png", img)
             count += 1
 
-            #保存した画像をold_imgに代入
+            # 保存した画像をold_imgに代入
             old_img = img
             print("Screenshot!")
 
         time.sleep(sleep_time)
 
 
-def ImageChangeRate(img1,img2,isShow=False):
+def ImageChangeRate(img1, img2, isShow=False):
     # 画像をグレースケールに変換
     img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
     img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
     # 2枚の画像の差分を求める
-    mdframe = cv2.absdiff(img1,img2,0.5)
+    mdframe = cv2.absdiff(img1, img2, 0.5)
     # 白と黒の2色の画像にする
     thresh = cv2.threshold(mdframe, 10, 255, cv2.THRESH_BINARY)[1]
 
-    #画像サイズ
+    # 画像サイズ
     image_size = thresh.size
-    #白のピクセル数／画像サイズ
+    # 白のピクセル数／画像サイズ
     whitePixels = cv2.countNonZero(thresh)
     whiteAreaRatio = (whitePixels/image_size)
 
@@ -57,13 +59,14 @@ def ImageChangeRate(img1,img2,isShow=False):
 
 
 def ScreenShot():
-    #スクリーンショット
+    # スクリーンショット
     img = pag.screenshot()
     img = img.convert('RGB')
-    #pyautoguiの画像をopencvの画像形式に変換
+    # pyautoguiの画像をopencvの画像形式に変換
     img = np.array(img)
-    img = img[:,:,::-1].copy()
+    img = img[:, :, ::-1].copy()
     return img
+
 
 if __name__ == "__main__":
     main()
